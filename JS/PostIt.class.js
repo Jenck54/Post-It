ad43c6c925bd2cde82f49f34eef95c2e7646c2f8
Jean Finck <jeanfinck29@gmail.com>
2021-11-15 16:26:22 +0100

    Jour1

--- JS/PostIt.class.js
+++ JS/PostIt.class.js
@@ -36,6 +36,7 @@ class PostIt {
             elem=document.createElement("div")
             document.body.appendChild(elem)
         }
+        elem.style.borderRadius = "10px"
         elem.style.position = "fixed"
         elem.style.width = this.largeur+"px"
         elem.style.height = this.hauteur+"px"
@@ -44,5 +45,69 @@ class PostIt {
         elem.style.left = this.x+"px"
         elem.innerHTML = this.texte
         elem.id = "PostIt"+this.id
+        let menu = document.createElement("div")
+        elem.appendChild(menu)
+        menu.className = "menu"
+        let btn1 = document.createElement("div")
+        btn1.className = "fas fa-arrows-alt"
+        menu.appendChild(btn1)
+        btn1.addEventListener("click", (evt)=>{
+            if(numPostIt==this.id) {
+                numPostIt = -1
+            }
+            else {
+                numPostIt = this.id
+            }
+            action = "deplace"
+            evt.stopPropagation()
+        })
+        let btn2 = document.createElement("div")
+        btn2.className = "fas fa-expand-arrows-alt"
+        menu.appendChild(btn2)
+        btn2.addEventListener("click",(evt)=>{
+            if(numPostIt==this.id) {
+                numPostIt = -1
+            }
+            else {
+                numPostIt = this.id
+            }
+            this.xorigin = evt.clientX
+            this.yorigin = evt.clientY
+            this.largeurorigin = this.largeur
+            this.hauteurorigin = this.hauteur
+            action = "redimensionne"
+            evt.stopPropagation()
+        })
+        let btn3 = document.createElement("div")
+        btn3.className = "fas fa-file-alt"
+        menu.appendChild(btn3)
+        btn3.addEventListener("click",(evt)=>{
+            numPostIt = this.id
+            evt.stopPropagation()
+            action = "ecrit"
+        })
+        let btn4 = document.createElement("div")
+        btn4.className = "fas fa-paint-roller"
+        menu.appendChild(btn4)
+        btn4.addEventListener("click",()=>{
+            if (this.couleur=="red") {
+                this.changerCouleur("blue")
+                this.affichePostIt()
+            }
+            else if (this.couleur=="blue") {
+                this.changerCouleur("yellow")
+                this.affichePostIt()
+            }
+            else if (this.couleur=="yellow") {
+                this.changerCouleur("red")
+                this.affichePostIt()
+            }
+        })
+        let btn5 = document.createElement("div")
+        btn5.className = "fas fa-trash-alt"
+        menu.appendChild(btn5)
+        btn5.addEventListener("click", (evt)=>{
+            
+        })
     }
 }
--- JS/script.js
+++ JS/script.js
@@ -1,10 +1,73 @@
-let testPostIt = new PostIt (50, 50, 200, 300, "red", "Ecrivez Ici", 1 )
-testPostIt.affichePostIt()
-testPostIt.deplacement(100, 200)
-testPostIt.affichePostIt()
-testPostIt.redimensionnement(100, 100)
-testPostIt.affichePostIt()
-testPostIt.changementText("Salut")
-testPostIt.affichePostIt()
-testPostIt.changerCouleur("blue")
-testPostIt.affichePostIt()
+let tablePostIt = []
+let numPostIt = -1
+let action = ""
+document.querySelector(".post1").addEventListener("click",()=>{
+    tablePostIt.push( new PostIt (450, 100, 250, 250, "red", "", tablePostIt.length ))
+    tablePostIt[tablePostIt.length-1].affichePostIt()
+})
+document.querySelector(".post2").addEventListener("click",()=>{
+    tablePostIt.push( new PostIt (450, 375, 250, 250, "blue", "", tablePostIt.length ))
+    tablePostIt[tablePostIt.length-1].affichePostIt()
+})
+document.querySelector(".post3").addEventListener("click",()=>{
+    tablePostIt.push( new PostIt (450, 650, 250, 250, "yellow", "", tablePostIt.length ))
+    tablePostIt[tablePostIt.length-1].affichePostIt()
+})
+document.body.addEventListener("keydown", (evt)=>{
+    console.log(evt.key)
+    if (numPostIt>-1 && action=="ecrit") {
+        if(evt.key=="Backspace"){
+    
+            tablePostIt[numPostIt].changementText(tablePostIt[numPostIt].texte.substr(0, tablePostIt[numPostIt].texte.length-1))
+            tablePostIt[numPostIt].affichePostIt()
+        }
+        else if(evt.key=="Delete"){  
+            tablePostIt[numPostIt].changementText(tablePostIt[numPostIt].texte.substr(0, tablePostIt[numPostIt].texte.length-1))
+            tablePostIt[numPostIt].affichePostIt()
+        }
+        else if(evt.key=="Enter"){  
+            tablePostIt[numPostIt].changementText(tablePostIt[numPostIt].texte+"<br>")
+            tablePostIt[numPostIt].affichePostIt()
+        }
+        else if(evt.key=="²"){
+        }
+        else if(evt.key=="Tab"){
+        }
+        else if(evt.key=="CapsLock"){
+        }
+        else if(evt.key=="Shift"){    
+        }
+        else if(evt.key=="Control"){
+        }
+        else if(evt.key=="Meta"){  
+        }
+        else if(evt.key=="Alt"){  
+        }
+        else if(evt.key=="AltGraph"){  
+        }
+        else if(evt.key=="ContextMenu"){  
+        }
+        else if(evt.key=="NumLock"){  
+        }
+        else if(evt.key=="Escape"){  
+        }
+        else {
+            tablePostIt[numPostIt].changementText(tablePostIt[numPostIt].texte+evt.key)
+            tablePostIt[numPostIt].affichePostIt()
+        }
+    }
+})
+document.body.addEventListener("mousemove",(evt)=>{
+    if(numPostIt>-1 && action=="deplace") {
+        tablePostIt[numPostIt].deplacement(evt.clientX-tablePostIt[numPostIt].largeur+220,evt.clientY-tablePostIt[numPostIt].hauteur+20)
+        tablePostIt[numPostIt].affichePostIt()
+    }
+    if(numPostIt>-1 && action=="redimensionne") {
+        tablePostIt[numPostIt].redimensionnement(evt.clientX-tablePostIt[numPostIt].xorigin+tablePostIt[numPostIt].largeurorigin,evt.clientY-tablePostIt[numPostIt].yorigin+tablePostIt[numPostIt].hauteurorigin)
+        tablePostIt[numPostIt].affichePostIt()
+    }
+})
+document.body.addEventListener("click",()=>{
+    numPostIt=-1
+    action = ""
+})
